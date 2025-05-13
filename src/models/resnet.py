fc18bf43e1b2b223be147badd37f48ffdd7b4d94
cpadubidri <chirag.padubidri@gmail.com>
2025-05-13 23:04:57 +0300

    Update sample model

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -10,14 +10,14 @@ class resnet18(nn.Module):
         super(resnet18,self).__init__()
 
         resnet = models.resnet18(weights='ResNet18_Weights.DEFAULT')
-        self.feature_extractor = nn.Sequential(*list(resnet.children())[:-1])
+        self.feature_extractor = nn.Sequential(*list(resnet.children())[:-1]) #>>> remove classification head
 
         self.embedding_head = nn.Sequential(
             nn.Flatten(),
             nn.Linear(512, 512),
             nn.ReLU(),
             nn.Linear(512, embedding_dim)
-        )
+        ) #create embedding head
 
     def forward(self, x):
         x = self.feature_extractor(x) #1024>>>512
