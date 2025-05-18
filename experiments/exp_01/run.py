a87c04e87401de74c90d5226c7a1f63a7055bf48
cpadubidri <chirag.padubidri@gmail.com>
2025-05-18 22:18:04 +0300

    Add experiment scripts

--- experiments/exp_01/run.py
+++ experiments/exp_01/run.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 
 sys.path.append(os.path.abspath(os.path.join('/home/savvas/SUPER-NAS/USERS/Chirag/PROJECTS/202504-spacenet9/data/spacenet9-void', 'src')))
-from datafeeder.datafeeder import get_dataloader
+from datafeeder import get_dataloader_v2
 from training import train
 from models import resnet18
 from utils import Configuration
@@ -18,11 +18,11 @@ if __name__=="__main__":
 
     config = Configuration('experiments/exp_01/config.json') 
 
-    train_loader = get_dataloader(config)
-    test_loader = get_dataloader(config)
+    train_loader = get_dataloader_v2(config, mode="train")
+    val_loader = get_dataloader_v2(config, mode="valid")
 
 
 
-    # train(config_path=config_path, model=model, 
-    #       train_loader=train_loader, 
-    #       test_loader=test_loader)
+    train(config_path=config_path, model=model, 
+          train_loader=train_loader, 
+          val_loader=val_loader)
