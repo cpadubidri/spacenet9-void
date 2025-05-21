87178429282c44b84371931c127070b5a15248f8
cpadubidri <chirag.padubidri@gmail.com>
2025-05-21 18:39:58 +0300

    Add resent50 triplet 2d model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
-from .resnet import resnet18, ResNet50Spatial
+from .resnet import resnet18, ResNet50Spatial, ResNet50Triplet2D
 from .unet import UNetHeatmap
 
 
-__all__ = ["resnet18","UNetHeatmap","ResNet50Spatial"]
+__all__ = ["resnet18","UNetHeatmap","ResNet50Spatial","ResNet50Triplet2D"]
