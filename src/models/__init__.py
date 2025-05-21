2e9b9b9b1d0c7a32189883a758ab25fba920ec26
Pranaam19 <22j34.pranam@sjec.ac.in>
2025-05-21 20:59:01 +0530

    updated __init__ file

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,4 @@
 from .resnet import resnet18, ResNet50Spatial
-from .unet import UNetHeatmap
 
 
-__all__ = ["resnet18","UNetHeatmap","ResNet50Spatial"]
+__all__ = ["resnet18","ResNet50Spatial"]
--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,5 +1,4 @@
 from .train import train
-from .train_unet import train_unet
 from .loss_fn import get_loss
 
-__all = ['train','get_loss','train_unet']
+__all__ = ['train','get_loss']
