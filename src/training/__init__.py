3665e1f81b490a581e3126cc9c19cfeddeaba89e
cpadubidri <chirag.padubidri@gmail.com>
2025-05-21 18:40:59 +0300

    Add triplet rain __init

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,5 +1,6 @@
 from .train import train
 from .train_unet import train_unet
+from .train_triplet import train_triplet
 from .loss_fn import get_loss
 
-__all = ['train','get_loss','train_unet']
+__all = ['train','get_loss','train_unet',"train_triplet"]
