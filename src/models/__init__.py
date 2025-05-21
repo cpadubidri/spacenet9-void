f22e614571231310230bd49a6386384ed6a77150
cpadubidri <chirag.padubidri@gmail.com>
2025-05-21 12:21:46 +0300

    Add py files

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,5 @@
-from .resnet import resnet18
+from .resnet import resnet18, ResNet50Spatial
+from .unet import UNetHeatmap
 
 
-__all__ = ["resnet18"]
+__all__ = ["resnet18","UNetHeatmap","ResNet50Spatial"]
--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,4 +1,5 @@
 from .train import train
+from .train_unet import train_unet
 from .loss_fn import get_loss
 
-__all = ['train','get_loss']
+__all = ['train','get_loss','train_unet']
