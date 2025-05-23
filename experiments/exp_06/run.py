91bd0c0dcce2e3a5d59b922fa8d1e6c663fa8445
cpadubidri <chirag.padubidri@gmail.com>
2025-05-23 10:54:23 +0300

    Add experiment6 results

---
+++ experiments/exp_06/run.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import numpy as np
+
+sys.path.append(os.path.abspath(os.path.join('/home/savvas/SUPER-NAS/USERS/Chirag/PROJECTS/202504-spacenet9/data/spacenet9-void', 'src')))
+from datafeeder import get_dataloader_v3
+from training import train_triplet
+from models import resnet18, ResNet50Triplet2D
+from utils import Configuration
+
+
+
+if __name__=="__main__":
+    config_path = "experiments/exp_06/config.json"
+
+
+    model = ResNet50Triplet2D(embedding_dim=128)
+
+    config = Configuration('experiments/exp_06/config.json') 
+
+    train_loader = get_dataloader_v3(config, mode="train")
+    val_loader = get_dataloader_v3(config, mode="valid")
+
+
+
+    train_triplet(config_path=config_path, model=model, 
+          train_loader=train_loader, 
+          val_loader=val_loader)
